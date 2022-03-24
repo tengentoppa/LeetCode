e33d4223e56ec1184a826f89acecd2bd2c2fa42c
simon <simonchen@gbd.com>
2022-03-24 22:52:10 +0800

    add leet 881

---
+++ dotNet/LeetCode/LeetCode/Solutions/S0881/Solution.cs
@@ -0,0 +1,57 @@
+﻿
+using LeetCode.Interface;
+
+namespace Solutions.S0881
+{
+    public class Solution : ILeet
+    {
+        readonly int[] people = { 1, 1, 1 };
+        readonly int limit = 3;
+
+        public object Input
+        {
+            get => new
+            {
+                people,
+                limit
+            };
+        }
+
+        public object Output
+        {
+            get => Run();
+        }
+
+        public object Run()
+        {
+            ISolution solution = new Solution1();
+
+            return solution.NumRescueBoats(people, limit);
+        }
+    }
+    public class Solution1 : ISolution
+    {
+        public int NumRescueBoats(int[] people, int limit)
+        {
+            Array.Sort(people);
+            var ans = 0;
+            var i = 0;
+            var j = people.Length - 1;
+            while (i <= j)
+            {
+                ans++;
+                if (people[i] + people[j] <= limit)
+                {
+                    i++;
+                }
+                j--;
+            }
+            return ans;
+        }
+    }
+
+    internal interface ISolution
+    {
+        int NumRescueBoats(int[] people, int limit);
+    }
+}
