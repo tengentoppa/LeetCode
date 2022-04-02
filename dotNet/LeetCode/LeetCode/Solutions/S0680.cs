dd5f204e6c706257aeaaced19320c7d4284bf8e8
Simon <tengentoppa@hotmail.com.tw>
2022-04-03 00:29:56 +0800

    add leet 680

---
+++ dotNet/LeetCode/LeetCode/Solutions/S0680.cs
@@ -0,0 +1,79 @@
+﻿
+using LeetCode.Interface;
+
+namespace Solutions.S0680
+{
+    public class Info : ILeetInfo
+    {
+        public string Problem => "Valid Palindrome II";
+
+        public TopicType[] Topics => new TopicType[]
+        {
+            TopicType.TwoPointers,
+            TopicType.String,
+            TopicType.Greedy
+        };
+    }
+    public class Solution : ILeet
+    {
+        readonly string s = "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga";
+
+        public object Input
+        {
+            get => new
+            {
+                s,
+            };
+        }
+
+        public object Output
+        {
+            get => Run();
+        }
+
+        public object Run()
+        {
+            ISolution solution = new Solution1();
+
+            return solution.ValidPalindrome(s);
+        }
+    }
+    public class Solution1 : ISolution
+    {
+        public bool ValidPalindrome(string s)
+        {
+            var left = 0;
+            var right = s.Length - 1;
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return CheckPalindrome(s, left + 1, right) || CheckPalindrome(s, left, right - 1);
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+        private bool CheckPalindrome(string s, int i, int j)
+        {
+            while (i < j)
+            {
+                if (s[i] != s[j])
+                {
+                    return false;
+                }
+
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+
+    internal interface ISolution
+    {
+        bool ValidPalindrome(string s);
+    }
+}
