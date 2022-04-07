1a236bc4a02bd3579a3ae8f3883e429abcd2d83e
Simon <tengentoppa@hotmail.com.tw>
2022-04-08 00:59:42 +0800

    add leet 921

---
+++ dotNet/LeetCode/LeetCode/Solutions/S0921.cs
@@ -0,0 +1,65 @@
+﻿
+using LeetCode.Interface;
+
+namespace Solutions.S0921
+{
+    public class Info : ILeetInfo
+    {
+        public string Problem => "Minimum Add to Make Parentheses Valid";
+
+        public Difficulty Difficulty => Difficulty.Medium;
+
+        public TopicType[] Topics => new TopicType[]
+        {
+            TopicType.String,
+            TopicType.Stack,
+            TopicType.Greedy
+        };
+    }
+
+    public class Solution : ILeet
+    {
+        readonly string s = "(())()(()()((()()()))((((";
+
+        public object Input
+        {
+            get => new
+            {
+                s
+            };
+        }
+
+        public object Output
+        {
+            get => Run();
+        }
+
+        public object Run()
+        {
+            ISolution solution = new Solution1();
+
+            return solution.MinAddToMakeValid(s);
+        }
+    }
+    public class Solution1 : ISolution
+    {
+        public int MinAddToMakeValid(string s)
+        {
+            var left = 0;
+            var invalid = 0;
+            foreach (var c in s)
+            {
+                if (c == '(') left++;
+                else if (c == ')' && left > 0) left--;
+                else invalid++;
+            }
+
+            return invalid + left;
+        }
+    }
+
+    internal interface ISolution
+    {
+        int MinAddToMakeValid(string s);
+    }
+}
